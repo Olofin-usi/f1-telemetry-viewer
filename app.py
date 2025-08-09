723ee7e3e42f84fe459501b90b0dff69b022f76e
Olofin .T <lilofu14@gmail.com>
2025-08-09 15:04:55 -0300

    adding more years to the app and also a track plotting feature

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.graph_objects as go
 from fastf1 import get_session
 import fastf1
+from  Track_plot import plot_track_map
 from plotly_functions import (
     plot_speed_plotly,
     plot_longitudinal_acceleration_plotly,
@@ -17,7 +18,7 @@ st.set_page_config(layout="wide", page_title="F1 Telemetry Viewer")
 
 st.title("🏎️ F1 Telemetry Visualizer")
 
-year = st.selectbox("Select Year", list(range(2021, 2025)))
+year = st.selectbox("Select Year", list(range(2019, 2025)))
 gp = st.text_input("Enter Grand Prix Name (e.g. 'Monza')", value="Monza")
 session_type = st.selectbox("Session", ["Q", "R", "FP1", "FP2", "FP3"])
 driver = st.text_input("Driver Code (e.g. 'VER')", value="VER")
@@ -28,7 +29,10 @@ if st.button("Load Data"):
         session.load()
         lap = session.laps.pick_driver(driver).pick_fastest()
         telemetry_driver = lap.get_car_data().add_distance()
-
+        
+        
+        plot_track_map(gp,session_type,year,driver, highlight_corners=True, title=None,
+                   color='white')
         plot_speed_plotly(telemetry_driver)
         plot_longitudinal_acceleration_plotly(telemetry_driver)
         plot_throttle_brake_plotly(telemetry_driver)
